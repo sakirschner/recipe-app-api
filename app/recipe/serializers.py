088c3e3beacef4b60081d0e47edcf7f8eaec6686
Scott Kirschner <scott.kirschner@perficient.com>
2020-11-30 08:55:06 -0500

    added upload recipe image feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,7 +38,17 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'price', 'link')
         read_only_fields = ('id',)
 
+
 class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializers for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
